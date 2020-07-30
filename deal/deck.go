fb3767111986540869a69181c4fe1b3609dc9f77
zshearin <zshearin617@gmail.com>
2020-07-30 11:44:23 -0400

    add deal func

--- deal/deck.go
+++ deal/deck.go
@@ -5,6 +5,7 @@ import (
 	"time"
 	"bytes"
 	"fmt"
+	"strconv"
 )
 
 //Card is a single element in the deck
@@ -13,8 +14,11 @@ type Card struct {
 	Value string
 }
 
-//Deck is a set of cards
-type Deck []Card
+
+type Deck struct {
+	Cards []Card
+	NextCardIndex int
+}
 
 //GetDeck returns a sorted deck of cards
 func GetDeck() Deck {
@@ -34,27 +38,27 @@ func GetDeck() Deck {
 				Suit:  suit,
 				Value: value,
 			}
-			deck = append(deck, currentCard)
+			deck.Cards = append(deck.Cards, currentCard)
 		}
 	}
-
+	deck.NextCardIndex = 0
 	return deck
 
 }
 
 //Shuffle shuffles a deck of cards
-func (d Deck) Shuffle() {
+func (d *Deck) Shuffle() {
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(d), func(i, j int) { d[i], d[j] = d[j], d[i] })
+	rand.Shuffle(len(d.Cards), func(i, j int) { d.Cards[i], d.Cards[j] = d.Cards[j], d.Cards[i] })
 }
 
 //PrintOrder prints the order of the deck
 func (d Deck) PrintOrder() {
 	var b bytes.Buffer
-	for index, card := range d {
+	for index, card := range d.Cards {
 		b.WriteString(card.Value)
 		b.WriteString(card.Suit)
-		
+
 		//b.WriteString("\n")
 		if (index +1) % 13 == 0 {
 			b.WriteString("\n")
@@ -62,6 +66,86 @@ func (d Deck) PrintOrder() {
 			b.WriteString(", ")
 		}
 	}
+        b.WriteString("\n")
+	fmt.Printf(b.String())
+}
+
+func (d Deck) PrintRemainingCards() {
+        var b bytes.Buffer
+        for index, card := range d.Cards {
+		if index < d.NextCardIndex {
+			continue
+		}
+                b.WriteString(card.Value)
+                b.WriteString(card.Suit)
+
+                //b.WriteString("\n")
+                if (index +1) % 13 == 0 {
+                        b.WriteString("\n")
+                } else {
+                        b.WriteString(", ")
+                }
+        }
+        b.WriteString("\n")
+        fmt.Printf(b.String())
+}
+
+
+func (d *Deck) DealHoldem(numHands int) Hands {
+	return d.Deal(numHands, 2)
+}
+
+type Hand []Card
+type Hands []Hand
+
+func (h Hands) PrintHands() {
+	var b bytes.Buffer
+	for index, hand := range h {
+		        b.WriteString("Hand ")
+                        b.WriteString(strconv.Itoa(index +1))
+                        b.WriteString(": ")
+
+		for i, card := range hand {
+			b.WriteString(card.Value)
+			b.WriteString(card.Suit)
+			if i != len(hand) - 1 {
+				b.WriteString(", ")
+			}
+		}
+		b.WriteString("\n")
+	}
+	b.WriteString("\n")
 
 	fmt.Printf(b.String())
 }
+
+func (d *Deck) Deal(numHands, numCards int) Hands {
+
+	//1 - create DS to store hands in 
+
+	//2 - iterate through and add cards to those hands
+
+
+	var hands Hands
+
+	for i := 0; i < numHands; i++ {
+
+		hand := []Card{}
+		hands = append(hands, hand)
+	}
+
+	for i := 0; i < numCards; i++ {
+
+		for j := 0; j < numHands; j++ {
+			curHand := hands[j]
+			curHand = append(curHand, d.Cards[d.NextCardIndex])
+
+			hands[j] = curHand 
+			d.NextCardIndex++
+		}
+	}
+
+
+	return hands
+}
+
