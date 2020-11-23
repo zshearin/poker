4814cb11b29e5eecfaf744d95805b4b16fedbb94
Zach-Shearin <zach.shearin@ibm.com>
2020-11-23 14:24:55 -0500

    change back to poker but refactor

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	poker "github.com/zshearin/poker/cmd/poker"
+)
+
+func main() {
+	game1 := shuffleAndDeal(5)
+	game1.PrintBoardAndHands()
+	game1.PrintBestFive()
+
+}
+
+func shuffleAndDeal(players int) poker.Game {
+
+	deck := poker.GetDeck()
+	deck.Shuffle()
+	deck.Shuffle()
+	//deck.PrintOrder()
+	return deck.GetGame(players)
+}
