7abba00a2b88cf24b131378cd63f8e979969b1c9
Zach Shearin <zshearin617@gmail.com>
2021-03-22 10:02:12 -0400

    Add Feature to Intentionally Crash Program After Random Amount of Time (#53)

--- cmd/main.go
+++ cmd/main.go
@@ -2,40 +2,75 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
+
+	"math/rand"
 
 	poker "github.com/zshearin/poker/cmd/poker"
 )
 
 func main() {
 
+	go handleServer()
+
+	crashTheProgram()
+}
+
+func handleServer() {
 	http.HandleFunc("/", pokerHandler)
-	log.Fatal(http.ListenAndServe(":4050", nil))
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+func crashTheProgram() {
+	rand.Seed(time.Now().UnixNano())
+	min := 5
+	max := 30
+
+	randInt := rand.Intn(max-min) + min
+	fmt.Print("Sleeping for " + strconv.Itoa(randInt) + " seconds before we crash")
+
+	time.Sleep(time.Duration(randInt) * time.Second)
+	fmt.Println("Crashing the program")
+	err := errors.New("this is a fake crash just demonstrating behavior")
+	panic(err)
 
 }
 
 func pokerHandler(w http.ResponseWriter, r *http.Request) {
-	deal1 := shuffleAndDeal(4)
 
-	deal1.PrintBoardAndHands()
-	deal1.Players.Print()
+	switch r.Method {
+	case "GET":
 
-	type result struct {
-		Board       poker.Cards
-		Hands       poker.Hands
-		HandResults []poker.HandResult
+		deal1 := shuffleAndDeal(4)
+
+		deal1.PrintBoardAndHands()
+		deal1.Players.Print()
+
+		type result struct {
+			Board       poker.Cards
+			Hands       poker.Hands
+			HandResults []poker.HandResult
+		}
+		res := result{
+			Board:       deal1.GetBoard(),
+			Hands:       deal1.Hands,
+			HandResults: deal1.HandResults,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(res)
+		//Now format the body with all the objects we want
+
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
 	}
-	res := result{
-		Board:       deal1.GetBoard(),
-		Hands:       deal1.Hands,
-		HandResults: deal1.HandResults,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(res)
-	//Now format the body with all the objects we want
 
 }
 
