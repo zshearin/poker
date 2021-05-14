519df0ca7e60c024c9ba89d6abac3d92e7d024d5
sugarcrm-zshearin <zach.shearin@sugarcrm.com>
2021-05-14 14:13:12 -0400

    remove crash program and AWS stuff

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # poker
 
-Current state: testing failures - I have it crashing after a given amount of time to test AWS functionality with crashing program
-
 This creates a poker game and evaluates what each hand is.  Right now it is a basic http server that returns the outcome of a hand for four players.  In the json return object currently:
 1. The Board in order (list of 5 Card objects: 5 cards - first 3 are flop, 4th is turn and 5th is the river)
 2. Hands dealt to each player (a list of the players hands.  Each player's hand is a list of 2 card objects)
--- cmd/main.go
+++ cmd/main.go
@@ -2,26 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
-	"time"
 
 	poker "github.com/zshearin/poker/cmd/poker"
 )
 
-var startTime time.Time
-
 func main() {
 
-	startTime = time.Now()
 
 	handleServer()
 
-	//crashTheProgram()
 }
 
 func handleServer() {
@@ -29,40 +20,16 @@ func handleServer() {
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
-func crashTheProgram() {
-	rand.Seed(time.Now().UnixNano())
-	min := 5
-	max := 30
-
-	randInt := rand.Intn(max-min) + min
-	fmt.Println("Sleeping for " + strconv.Itoa(randInt) + " seconds before we crash")
-
-	time.Sleep(time.Duration(randInt) * time.Second)
-	fmt.Println("Crashing the program")
-	err := errors.New("this is a fake crash just demonstrating behavior")
-	panic(err)
-
-}
-
 func pokerHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
 
-		curTime := time.Now()
-		elapsed := curTime.Sub(startTime)
-
-		secondsTillFailure := 30
-		if elapsed > time.Duration(secondsTillFailure)*time.Second {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("its failing now, application has been running for " + elapsed.String() + " and threshold set at " + strconv.Itoa(secondsTillFailure) + " seconds"))
-			return
-		}
 
 		deal1 := shuffleAndDeal(4)
 
-		deal1.PrintBoardAndHands()
-		deal1.Players.Print()
+		//deal1.PrintBoardAndHands()
+		//deal1.Players.Print()
 
 		type result struct {
 			Board       poker.Cards
