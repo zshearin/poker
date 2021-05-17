78df43dc9e8e0f78b703b41459c1852912dff4d5
sugarcrm-zshearin <zach.shearin@sugarcrm.com>
2021-05-17 08:43:06 -0400

    add handling protobuf for GRPC

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
+	"net"
 	"net/http"
 
-	poker "github.com/zshearin/poker/cmd/poker"
+	//	"net"
+
+	//	"google.golang.org/grpc"
+
+	"github.com/zshearin/poker/cmd/poker"
+	proto "github.com/zshearin/poker/cmd/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 
-
 	handleServer()
+
+	lis, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+
+	srv := grpc.NewServer()
+	proto.RegisterPokerServiceServer(srv, &server{})
+	reflection.Register(srv)
+	if e := srv.Serve(lis); e != nil {
+		panic(e)
+	}
+	/*
+		lis, err := net.Listen("tcp", ":9000")
+		if err != nil {
+			log.Fatalf("failed to listen on port 9000: %v", err)
+		}
+
+		s := poker.Server{}
+
+		grpcServer := grpc.NewServer()
+
+		//poker.RegisterChatServiceServer(grpcServer, &s)
+
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve gRPC server over port 9000: %v", err)
+		}
+	*/
 
 }
 
@@ -24,7 +60,6 @@ func pokerHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
-
 
 		deal1 := shuffleAndDeal(4)
 
@@ -60,4 +95,67 @@ func shuffleAndDeal(players int) poker.Deal {
 	deck.Shuffle()
 	//deck.PrintOrder()
 	return deck.GetDeal(players)
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+type server struct {
+}
+
+func (s *server) GetDeal(ctx context.Context, request *proto.Request) (*proto.Deal, error) {
+	numHandsInDeal := request.GetHands()
+
+	deal := shuffleAndDeal(int(numHandsInDeal))
+
+	board := deal.GetBoard()
+	hands := deal.Hands
+	handResults := deal.HandResults
+
+	var newBoard []*proto.Card
+
+	for _, card := range board {
+		suit := card.Suit
+		value := card.Value
+		var newCard *proto.Card
+		newCard.Suit = suit
+		newCard.Value = value
+		newBoard = append(newBoard, newCard)
+	}
+
+	var newHands []*proto.Hand
+
+	for _, hand := range hands {
+
+		var curNewHand *proto.Hand
+
+		for _, card := range hand {
+			var curNewCard *proto.Card
+
+			curNewCard.Suit = card.Suit
+			curNewCard.Value = card.Value
+			curNewHand.Hand = append(curNewHand.Hand, curNewCard)
+		}
+
+		newHands = append(newHands, curNewHand)
+	}
+
+	var newResults []*proto.HandResult
+
+	for _, result := range handResults {
+		var newResult *proto.HandResult
+
+		newResult.HandName = result.HandName
+		newResult.Player = int32(result.PlayerNumber)
+
+		newResults = append(newResults, newResult)
+
+	}
+
+	return &proto.Deal{
+		Board:   newBoard,
+		Hands:   newHands,
+		Results: newResults,
+	}, nil
+
 }
