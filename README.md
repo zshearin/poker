c1c8d73bbe4451e3f5d85212a49521c639552836
Zach Shearin <zshearin617@gmail.com>
2021-08-27 15:08:16 -0400

    add input for number of hands and have default if nothing passed in (#64)
    * add input for number of hands and have default if nothing passed in
    
    * make printing look better and minor refactor for cleaner code
    
    * minor change

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # poker
 
-This creates a poker game and evaluates what each hand is.  Right now it is a basic http server that returns the outcome of a hand for four players.  In the json return object currently:
-1. The Board in order (list of 5 Card objects: 5 cards - first 3 are flop, 4th is turn and 5th is the river)
-2. Hands dealt to each player (a list of the players hands.  Each player's hand is a list of 2 card objects)
-3. Hand results (Ordered list in the winning hands with the player number and a description of their hand)
+This creates a poker game and prints out the results.
--- cmd/dealer/print.go
+++ cmd/dealer/print.go
@@ -7,6 +7,10 @@ import (
 	"strings"
 )
 
+var (
+	MIN_SPACE = 16
+)
+
 //PrintOrder prints the order of the deck
 func (d Deck) PrintOrder() {
 	var b bytes.Buffer
@@ -22,8 +26,7 @@ func (d Deck) PrintOrder() {
 			b.WriteString(", ")
 		}
 	}
-	b.WriteString("\n")
-	fmt.Printf(b.String())
+	fmt.Println(b.String())
 }
 
 //PrintRemainingCards prints the remaining cards in the deck
@@ -47,14 +50,19 @@ func (d Deck) PrintRemainingCards() {
 			b.WriteString(", ")
 		}
 	}
-	b.WriteString("\n")
-	fmt.Printf(b.String())
+	fmt.Println(b.String())
 }
 
 //Print prints hands
 func (h Hands) Print() {
 	for index, hand := range h {
-		hand.Print("Hand "+strconv.Itoa(index+1), "")
+		playerStr := strconv.Itoa(index + 1)
+
+		//Add space to make formatting prettier if 10 handed - 2 chars vs 1 in digit
+		if len(playerStr) == 1 {
+			playerStr = " " + playerStr
+		}
+		hand.Print("Hand "+playerStr, "")
 	}
 	fmt.Printf("\n")
 }
@@ -72,17 +80,16 @@ func (c Cards) Print(beforeStr, afterStr string) {
 		}
 		b.WriteString(card.Suit)
 		if index != len(c)-1 {
-			b.WriteString("  ")
+			b.WriteString(", ")
 		}
 
 	}
 	b.WriteString(afterStr)
-	b.WriteString("\n")
-	fmt.Printf(b.String())
+	fmt.Println(b.String())
 
 }
 
-func (c Cards) BasicPrint() {
+func (c Cards) getBestFiveString() string {
 	var b bytes.Buffer
 	for index, card := range c {
 		if card.Value == "1" {
@@ -96,24 +103,20 @@ func (c Cards) BasicPrint() {
 		}
 
 	}
-	fmt.Printf(b.String())
+	return b.String()
 }
 
 //Print prints players
 func (p Players) Print() {
 	for i := 0; i < len(p); i++ {
 		playerNum := p[i].Num
-
 		p[i].BestFive.Print("Player "+strconv.Itoa(playerNum), " ("+p[i].HandName+")")
 	}
-
 }
 
 //PrintBoard prints the board for a game
 func (d *Deal) PrintBoard() {
-
 	board := d.GetBoard()
-
 	board.Print("Board", "")
 }
 
@@ -123,16 +126,60 @@ func (d *Deal) PrintHands() {
 }
 
 func (d *Deal) PrintRanksAndBestFive() {
+	fmt.Println("Results:")
+	fmt.Println("====================================================")
+	fmt.Println("| Player | Rank |    Best Five   |     Hand Name   |")
+	fmt.Println("====================================================")
 
 	for _, handResult := range d.HandResults {
-		curPlayer := handResult.Player
-		fmt.Printf("Player %d rank: %d. Hand name: %s.", curPlayer.Num, handResult.RelativeHandRank, curPlayer.HandName)
-
-		spaces := strings.Repeat(" ", MIN_SPACE-len(curPlayer.HandName))
-		fmt.Printf("%s Best Five: ", spaces)
-		curPlayer.BestFive.BasicPrint()
-		fmt.Printf("\n")
+		line := getLineToPrint(handResult)
+		fmt.Println(line)
 	}
+	fmt.Println("----------------------------------------------------")
+}
+
+func getLineToPrint(handResult HandResult) string {
+	curPlayer := handResult.Player
+
+	//Add space to make formatting prettier if 10 handed - 2 chars vs 1 in digit
+	playerNumStr := strconv.Itoa(curPlayer.Num)
+	if len(playerNumStr) == 1 {
+		playerNumStr = playerNumStr + " "
+	}
+
+	//Add space to make formatting prettier if 10 handed - 2 chars vs 1 in digit
+	handRankStr := strconv.Itoa(handResult.RelativeHandRank)
+	if len(handRankStr) == 1 {
+		handRankStr = " " + handRankStr
+	}
+
+	bestFiveStr := curPlayer.BestFive.getBestFiveString()
+
+	//Calculate spaces before and after hand name for prettier formatting
+	numSpaces := MIN_SPACE - len(curPlayer.HandName)
+	spacesBefore := strings.Repeat(" ", numSpaces/2)
+	var spacesAfter string
+	if numSpaces%2 == 0 {
+		spacesAfter = strings.Repeat(" ", numSpaces/2)
+	} else {
+		spacesAfter = strings.Repeat(" ", numSpaces/2+1)
+
+	}
+
+	//Build output string and return
+	var sb strings.Builder
+	sb.WriteString("|   ")
+	sb.WriteString(playerNumStr)
+	sb.WriteString("   |  ")
+	sb.WriteString(handRankStr)
+	sb.WriteString("  | ")
+	sb.WriteString(bestFiveStr)
+	sb.WriteString(" | ")
+	sb.WriteString(spacesBefore)
+	sb.WriteString(curPlayer.HandName)
+	sb.WriteString(spacesAfter)
+	sb.WriteString("|")
+	return sb.String()
 }
 
 //PrintBoardAndHands prints the board and the hands
